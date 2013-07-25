b9a61a15f44c31a0b928ba4daf5e3be5e2c64f09
Sneha Bhattacharya <sneha.22.7@gmail.com>
2013-07-25 18:38:12 +0530

    admin ui

---
+++ vesuvius/demomanager.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+if( (isset($_POST['shortname'])) && (isset($_POST['dbname'])) && (isset($_POST['pswrd'])) && (isset($_POST['hname'])) && (isset($_POST['dbUser'])))
+{
+$u=$_POST['shortname'];
+$t=$_POST['dbname'];
+$s=$_POST['pswrd'];
+$w=$_POST['hname'];
+$v=$_POST['dbUser'];
+$output =shell_exec("sh /home/sneha/clone.sh $u $t $s $w $v");
+//$output =shell_exec("echo " . $uname  );
+var_dump ($output);
+}
+
+//shell_exec('sh /home/sneha/clone.sh " $uname "');
+//echo "<pre>$output</pre>";
+//$output = array();
+//exec('sh /home/sneha/clone.sh', $output);
+//var_dump($output);
+
+
+
+
+
+?>
+
